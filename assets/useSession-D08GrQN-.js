32460345ddecfa155638d8e1f9a980b28d844a63
Tamachiii <Tamachiii@users.noreply.github.com>
2026-04-15 22:04:00 +0000

    deploy: cd32585d9c6eaf9b7aeb8b933b30df60685c5c20

--- assets/useSession-D08GrQN-.js
+++ assets/useSession-D08GrQN-.js
@@ -1,4 +1,4 @@
-import{c as q,u,b as a}from"./query-BR4_tDY9.js";import{s as o}from"./index-088dxnmF.js";function m(s){return q({queryKey:["session",s],queryFn:async()=>{const{data:e,error:r}=await o.from("sessions").select(`
+import{c as q,u,b as a}from"./query-BR4_tDY9.js";import{s as o}from"./index-C32kLWQ2.js";function m(s){return q({queryKey:["session",s],queryFn:async()=>{const{data:e,error:r}=await o.from("sessions").select(`
           *,
           exercise_slots(
             *,
