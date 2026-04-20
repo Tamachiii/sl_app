e7000f413e54b808994c0671ab1f60ca00b7e83b
Tamachiii <Tamachiii@users.noreply.github.com>
2026-04-20 10:17:36 +0000

    deploy: f0d617cfd162f891e48d75f05e34e8058eb9a0c8

--- assets/useWeek-05RLLUUj.js
+++ assets/useWeek-05RLLUUj.js
@@ -1,4 +1,4 @@
-import{c as y,u as a,b as u}from"./query-BR4_tDY9.js";import{s as n}from"./index-Cuq8XTER.js";function q(r){return y({queryKey:["week",r],queryFn:async()=>{const{data:e,error:s}=await n.from("weeks").select("*").eq("id",r).single();if(s)throw s;const{data:i,error:t}=await n.from("sessions").select(`
+import{c as y,u as a,b as u}from"./query-BR4_tDY9.js";import{s as n}from"./index-CX0w_yG3.js";function q(r){return y({queryKey:["week",r],queryFn:async()=>{const{data:e,error:s}=await n.from("weeks").select("*").eq("id",r).single();if(s)throw s;const{data:i,error:t}=await n.from("sessions").select(`
           *,
           exercise_slots(
             *,
