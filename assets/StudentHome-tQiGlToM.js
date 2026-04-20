b8c7f59e7622848cc0d957f3e6d3c93b8a22b475
Tamachiii <Tamachiii@users.noreply.github.com>
2026-04-20 11:58:04 +0000

    deploy: ec704874755750fe0b8b72e6bf23bf46c243c2ab

--- assets/StudentHome-tQiGlToM.js
+++ assets/StudentHome-tQiGlToM.js
@@ -1,4 +1,4 @@
-import{c as w,j as e}from"./query-IY3PWAwZ.js";import{d as N,r as p}from"./router-QPrNqfwh.js";import{H as f}from"./Header-XetROFYi.js";import{s as y,u as _,S as k}from"./index-DgS_jFjH.js";import{c as v}from"./useSessionConfirmation-gc-6SvOW.js";import{E as j}from"./EmptyState-B5kL0KxI.js";import"./supabase-k82gbVKr.js";function S(s){return w({queryKey:["student-weeks",s],queryFn:async()=>{const{data:d,error:r}=await y.from("students").select("id").eq("profile_id",s).single();if(r)throw r;const{data:i,error:n}=await y.from("programs").select(`
+import{c as w,j as e}from"./query-IY3PWAwZ.js";import{d as N,r as p}from"./router-QPrNqfwh.js";import{H as f}from"./Header-Du8lVO4F.js";import{s as y,u as _,S as k}from"./index-DdfkTIq4.js";import{c as v}from"./useSessionConfirmation-CQZqcUob.js";import{E as j}from"./EmptyState-B5kL0KxI.js";import"./supabase-k82gbVKr.js";function S(s){return w({queryKey:["student-weeks",s],queryFn:async()=>{const{data:d,error:r}=await y.from("students").select("id").eq("profile_id",s).single();if(r)throw r;const{data:i,error:n}=await y.from("programs").select(`
           id, name,
           weeks(
             id, week_number, label,
