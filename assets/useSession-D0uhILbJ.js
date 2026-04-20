61287476d3c4760077a0404bcf996d23cbb1f1bd
Tamachiii <Tamachiii@users.noreply.github.com>
2026-04-20 09:47:40 +0000

    deploy: b4fec69394a740cb065612710dbf7e88c59b43d7

--- assets/useSession-D0uhILbJ.js
+++ assets/useSession-D0uhILbJ.js
@@ -1,4 +1,4 @@
-import{c as q,u,b as a}from"./query-BR4_tDY9.js";import{s as o}from"./index-CTqy0r80.js";function m(s){return q({queryKey:["session",s],queryFn:async()=>{const{data:e,error:r}=await o.from("sessions").select(`
+import{c as q,u,b as a}from"./query-BR4_tDY9.js";import{s as o}from"./index-yWOXpJTz.js";function m(s){return q({queryKey:["session",s],queryFn:async()=>{const{data:e,error:r}=await o.from("sessions").select(`
           *,
           exercise_slots(
             *,
