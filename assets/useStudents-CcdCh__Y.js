f958289d7851f8a290dcc7ead5c5914a974cbe57
Tamachiii <Tamachiii@users.noreply.github.com>
2026-04-20 22:05:44 +0000

    deploy: db7892fa7ff2c3b34354a5805657fa86a2d52f1c

--- assets/useStudents-CcdCh__Y.js
+++
@@ -1,4 +0,0 @@
-import{c as t}from"./query-BBurVEW5.js";import{u,s as a}from"./index-Cx_p3TKl.js";function f(){const{user:r}=u();return t({queryKey:["students"],queryFn:async()=>{const{data:s,error:e}=await a.from("students").select(`
-          *,
-          profile:profiles!students_profile_id_fkey(full_name)
-        `).order("created_at");if(e)throw e;return s},enabled:!!r})}export{f as u};
