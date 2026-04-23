54a64998496bb5b6af2cf413a8f73138ba9c6097
Tamachiii <Tamachiii@users.noreply.github.com>
2026-04-23 13:38:32 +0000

    deploy: ab2ec5fd7cab54ae3950303a343b70883bb42559

--- assets/useSession-DuwsrRWT.js
+++ assets/useSession-DuwsrRWT.js
@@ -1,4 +1,4 @@
-import{c as q,u,b as a}from"./query-sasIS2ap.js";import{s as o}from"./index-gZ1aQ_1T.js";function m(s){return q({queryKey:["session",s],queryFn:async()=>{const{data:e,error:r}=await o.from("sessions").select(`
+import{c as q,u,b as a}from"./query-sasIS2ap.js";import{s as o}from"./index-BRilp5sF.js";function m(s){return q({queryKey:["session",s],queryFn:async()=>{const{data:e,error:r}=await o.from("sessions").select(`
           *,
           exercise_slots(
             *,
