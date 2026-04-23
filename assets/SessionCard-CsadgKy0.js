1ea41cd876498bafff90d6e33ef6f03aa1a213f2
Tamachiii <Tamachiii@users.noreply.github.com>
2026-04-23 12:57:30 +0000

    deploy: 9ae56c64b92b46aaf79e85089148ed93487f9cbf

--- assets/SessionCard-CsadgKy0.js
+++ assets/SessionCard-CsadgKy0.js
@@ -1,4 +1,4 @@
-import{c as _,j as e}from"./query-sasIS2ap.js";import{s as f,a as j}from"./index-CXYRs3Wc.js";import{r as g}from"./router-CA_rAFgT.js";import{f as w}from"./volume-BZ-GBXEz.js";function C(s){return _({queryKey:["student-program-details",s],queryFn:async()=>{const{data:n,error:a}=await f.from("students").select("id").eq("profile_id",s).single();if(a)throw a;const{data:x,error:l}=await f.from("programs").select(`
+import{c as _,j as e}from"./query-sasIS2ap.js";import{s as f,a as j}from"./index-CT69Y9-u.js";import{r as g}from"./router-CA_rAFgT.js";import{f as w}from"./volume-BZ-GBXEz.js";function C(s){return _({queryKey:["student-program-details",s],queryFn:async()=>{const{data:n,error:a}=await f.from("students").select("id").eq("profile_id",s).single();if(a)throw a;const{data:x,error:l}=await f.from("programs").select(`
           id,
           weeks(
             id, week_number, label,
