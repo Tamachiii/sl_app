85407e8326556c1111249e461cb959876ae58590
Tamachiii <Tamachiii@users.noreply.github.com>
2026-04-24 22:15:35 +0000

    deploy: b7ba5f4e5ef4dfd69e4575e606562c3f7baa789e

--- assets/useSession-DqMBSwv_.js
+++ assets/useSession-DqMBSwv_.js
@@ -1,4 +1,4 @@
-import{c as q,u,b as a}from"./query-BK75LP8B.js";import{s as o}from"./index-Ct52uuZX.js";function m(s){return q({queryKey:["session",s],queryFn:async()=>{const{data:e,error:r}=await o.from("sessions").select(`
+import{c as q,u,b as a}from"./query-BK75LP8B.js";import{s as o}from"./index-uP_AmaLd.js";function m(s){return q({queryKey:["session",s],queryFn:async()=>{const{data:e,error:r}=await o.from("sessions").select(`
           *,
           exercise_slots(
             *,
